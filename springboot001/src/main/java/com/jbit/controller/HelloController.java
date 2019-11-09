d5ab257123b03c92c7b21791eb68dc5402ac3d8d
liujian <liujian2414561093@163.com>
2019-11-09 10:13:34 +0800

    添加controller测试代码

---
+++ springboot001/src/main/java/com/jbit/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.jbit.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String show(){
+        return "helloworld";
+    }
+}
